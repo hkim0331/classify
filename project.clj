fce4d60108c6c8079823eab89e99743b412a762e
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2024-06-10 08:52:34 +0900

    commit from m64

--- project.clj
+++ project.clj
@@ -39,22 +39,18 @@
                  [thheller/shadow-cljs "2.19.8" :scope "provided"]]
 
   :min-lein-version "2.0.0"
-  
   :source-paths ["src/clj" "src/cljs" "src/cljc"]
   :test-paths ["test/clj"]
   :resource-paths ["resources" "target/cljsbuild"]
   :target-path "target/%s/"
   :main ^:skip-aot classify.core
 
-  :plugins [] 
+  :plugins []
   :clean-targets ^{:protect false}
   [:target-path "target/cljsbuild"]
-  
 
   :profiles
   {:uberjar {:omit-source true
-             
-             :prep-tasks ["compile" ["run" "-m" "shadow.cljs.devtools.cli" "release" "app"]]
              :aot :all
              :uberjar-name "classify.jar"
              :source-paths ["env/prod/clj"  "env/prod/cljs" ]
@@ -73,9 +69,7 @@
                                  [ring/ring-mock "0.4.0"]]
                   :plugins      [[com.jakemccrary/lein-test-refresh "0.25.0"]
                                  [jonase/eastwood "1.2.4"]
-                                 [cider/cider-nrepl "0.28.5"]] 
-                  
-                  
+                                 [cider/cider-nrepl "0.28.5"]]
                   :source-paths ["env/dev/clj"  "env/dev/cljs" "test/cljs" ]
                   :resource-paths ["env/dev/resources"]
                   :repl-options {:init-ns user
@@ -83,9 +77,7 @@
                   :injections [(require 'pjstadig.humane-test-output)
                                (pjstadig.humane-test-output/activate!)]}
    :project/test {:jvm-opts ["-Dconf=test-config.edn" ]
-                  :resource-paths ["env/test/resources"] 
-                  
-                  
+                  :resource-paths ["env/test/resources"]
                   }
    :profiles/dev {}
    :profiles/test {}})
--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ export BASE="/Users/hkim/ramdisk/"
 export SRC="unsorted/"
 export DESTS='{"A" "a/" "B" "b/" "C" "c/" "D" "d/" "Other" "other/"}'
 
-java -jar target/uberjar/classify.jar
+java -jar target/default+uberjar/classify.jar
